b47e6b0ea4256d16071ba8f79fb0e95b0e729fb3
Samantha Davis <samantha.shea.davis@gmail.com>
2017-03-03 13:48:45 -0500

    unique product title unit test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -42,5 +42,13 @@ class ProductTest < ActiveSupport::TestCase
       bad.each do |name|
         assert new_product(name).invalid?, "#{name} shouldn't be valid"
       end
-    end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy", price: 1,
+                          image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
 end
